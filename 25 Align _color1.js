a87bc2cb5c0fc56e26f2eee53e259622137c48a3
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:39:23 +0500

    Add files via upload

---
+++ 25 Align _color1.js
@@ -0,0 +1,13 @@
+// creat variable called alien_color
+var alien_color = "green";
+// Write one version of this program that passes the if test and another that fails. 
+// The version that fails will have no output.)
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+// Write one version of this program that passes the if test and another that fails. 
+alien_color = "Red";
+// The version that fails will have no output.) 
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
---
+++ app..ts
@@ -0,0 +1,23 @@
+// Alien Colors #1: Imagine an alien was just shot down in a game.
+//  Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+
+// creat variable called alien_color
+let alien_color: string= "green";
+
+// Write one version of this program that passes the if test and another that fails. 
+// The version that fails will have no output.)
+if(alien_color =="green"){
+    console.log( "the player just earned 5 points.");
+
+}
+
+ 
+// Write one version of this program that passes the if test and another that fails. 
+alien_color = "Red";
+// The version that fails will have no output.) 
+ if(alien_color =="green"){
+  console.log( "the player just earned 5 points.");
+
+ }
+
