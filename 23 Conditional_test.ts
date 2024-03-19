098686e5faf22fe09bed1d189f8e5e94292b296c
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:35:31 +0500

    Add files via upload

---
+++ 23 Conditional_test.ts
@@ -0,0 +1,44 @@
+// test Conditional Tests: Write a series of conditional tests. 
+
+let car: string = "Civic";
+
+//test no 1 equility compresion 1st test (true)
+console.log("Is car == 'Civic'? I predict True.");
+console.log(car == 'Civic'); //true   
+
+// tset no 2 strict  equility compresion 2nd test (true)
+console.log("Is car === 'Civic'? I predict true.");
+console.log(car === 'Civic'); //true
+
+//test no 3 inequility comperison 3rd test (false)
+console.log("Is car != 'Civic'? I predict false.");
+console.log(car != 'Civic'); //false
+
+//test no 4 strict inequility comperison (false)
+console.log("Is car !== 'Civic'? I predict false.");
+console.log(car !== 'Civic'); //false
+
+
+//test no 5 less then comperison 5th test (false)
+console.log("Is car < 'Civic'? I predict false.");
+console.log(car < 'Civic'); //false lexicography compresion
+
+//test no 6 greator then comperison 6th test (false)
+console.log("Is car >'Civic'? I predict false.");
+console.log(car > 'Civic'); //false lexicography compresion
+
+// test no 7 less then or Equal comparesion (true)
+console.log("Is car <= 'Civic'? I predict True.");
+console.log(car <= 'Civic');
+
+// test no 8 greator then or Equal comparesion (true)
+console.log("Is car >= 'Civic'? I predict True.");
+console.log(car >= 'Civic'); (true)
+
+// test no 9 checking truthiness (true)
+console.log("Is car ?'Civic'? I predict True.");
+console.log(car); //true (non-empty string is truth)
+
+// test no 10 checking falsiness (false)
+console.log("Is car ?'Civic'? I predict false.");
+console.log(car);
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-"use strict";
-// Intentional Error: If you haven’t received an array index error in one of your programs yet, 
-// try to make one happen. Change an index in one of your programs to produce an index error. 
-// Make sure you correct the error before closing the program.
-let array = ['FARHANA', ' LAIBA', 'AREEBA', 7, 8, 9,];
-console.log(array[10]);
-console.log(array[5]);
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
-console.log(array[3]);
-console.log(array[4]);
-console.log(array[5]);
+// test Conditional Tests: Write a series of conditional tests. 
+var car = "Civic";
+//test no 1 equility compresion 1st test (true)
+console.log("Is car == 'Civic'? I predict True.");
+console.log(car == 'Civic'); //true   
+// tset no 2 strict  equility compresion 2nd test (true)
+console.log("Is car === 'Civic'? I predict true.");
+console.log(car === 'Civic'); //true
+//test no 3 inequility comperison 3rd test (false)
+console.log("Is car != 'Civic'? I predict false.");
+console.log(car != 'Civic'); //false
+//test no 4 strict inequility comperison (false)
+console.log("Is car !== 'Civic'? I predict false.");
+console.log(car !== 'Civic'); //false
+//test no 5 less then comperison 5th test (false)
+console.log("Is car < 'Civic'? I predict false.");
+console.log(car < 'Civic'); //false lexicography compresion
+//test no 6 greator then comperison 6th test (false)
+console.log("Is car >'Civic'? I predict false.");
+console.log(car > 'Civic'); //false lexicography compresion
+// test no 7 less then or Equal comparesion (true)
+console.log("Is car <= 'Civic'? I predict True.");
+console.log(car <= 'Civic');
+// test no 8 greator then or Equal comparesion (true)
+console.log("Is car >= 'Civic'? I predict True.");
+console.log(car >= 'Civic');
+(true);
+// test no 9 checking truthiness (true)
+console.log("Is car ?'Civic'? I predict True.");
+console.log(car); //true (non-empty string is truth)
+// test no 10 checking falsiness (false)
+console.log("Is car ?'Civic'? I predict false.");
+console.log(car);
