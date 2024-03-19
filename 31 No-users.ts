1f10dbaf8124dabe90b5aa0ceffdd7a9536b9090
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:44:26 +0500

    Add files via upload

---
+++ 31 No-users.ts
@@ -0,0 +1,53 @@
+//set a value for the variable age, and then:
+let age: number = 25;
+
+// If the person is less than 2 years old, print a message that the person is a baby
+// If the person is at least 2 years old but less than 4, print a message that the person is a toddler
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+// • If the person is age 65 or older, print a message that the person is an elder.
+if(age < 2){
+    console.log(" person is a baby.");
+}
+else if(age >= 2 && age < 4){
+    console.log("person is a toddler.");
+}
+else if(age >= 4 && age < 13 ){
+    console.log("person is a kid.");
+}
+
+else if(age >= 13 && age < 20 ){
+    console.log("person is a teenager.");
+}
+
+else if(age >= 20 && age < 65 ){
+    console.log(" person is an adult.");
+}
+else{
+    console.log("person is an elder.");
+}
+
+// test 31  No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+
+// Declare an empty array of strings for users
+let users: string[] = []; 
+
+
+// Check if the array is empty
+if (users.length === 0) {
+    console.log("We need to find some users!"); // Print message if array is empty
+} else {
+    // Iterate through the array and remove all usernames
+    users.forEach(function(user) {
+        console.log("Removing user: " + user); // Log the removal of each user
+    });
+
+    users = []; // Empty the array after removing all usernames
+    console.log("All users have been removed."); // Print confirmation message
+}
+
--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array,
-//  and print a greeting to each user:
-// test 30 
-var usernames = ['admin', 'FARHANA', 'LAIBA', 'AREEBA', 'SHAMA', 'Eric'];
-for (var _i = 0, usernames_1 = usernames; _i < usernames_1.length; _i++) {
-    var username = usernames_1[_i];
-    if (username.toLowerCase() === 'admin') {
-        console.log("Hello ".concat(username, ", would you like to see a status report?"));
-    }
-    else {
-        if (username.toLowerCase() === 'eric') {
-            console.log("Hello ".concat(username, ", thank you for logging in again."));
-        }
-        else {
-            console.log("Hello ".concat(username, ", Welcome back to the website !"));
-        }
-    }
+//set a value for the variable age, and then:
+var age = 25;
+// If the person is less than 2 years old, print a message that the person is a baby
+// If the person is at least 2 years old but less than 4, print a message that the person is a toddler
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+// • If the person is age 65 or older, print a message that the person is an elder.
+if (age < 2) {
+    console.log(" person is a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("person is a toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("person is a kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("person is a teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log(" person is an adult.");
+}
+else {
+    console.log("person is an elder.");
+}
+// test 31  No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+// Declare an empty array of strings for users
+var users = [];
+// Check if the array is empty
+if (users.length === 0) {
+    console.log("We need to find some users!"); // Print message if array is empty
+}
+else {
+    // Iterate through the array and remove all usernames
+    users.forEach(function (user) {
+        console.log("Removing user: " + user); // Log the removal of each user
+    });
+    users = []; // Empty the array after removing all usernames
+    console.log("All users have been removed."); // Print confirmation message
 }
