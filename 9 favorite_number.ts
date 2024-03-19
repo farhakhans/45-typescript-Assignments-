34a52df9ef193f386bd3a120277b664b09dc6f7e
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:20:39 +0500

    Add files via upload

---
+++ 9 favorite_number.ts
@@ -0,0 +1,17 @@
+// Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+
+// let favoriteNumber: Number= 4;
+// console.log(`My favorite number is ${favoriteNumber}`);
+
+// adding commints:
+// my name is farhana
+// date 16/03/2024
+// simple programme
+
+ let array: string []= ["farhana", "6", "2","2024"];
+
+console.log(array);
+
---
+++ favorite_number.js
@@ -0,0 +1,11 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+// let favoriteNumber: Number= 4;
+// console.log(`My favorite number is ${favoriteNumber}`);
+// adding commints:
+// my name is farhana
+// date 16/03/2024
+// simple programme
+let array = ["farhana", "6", "2", "2024"];
+console.log(array);
