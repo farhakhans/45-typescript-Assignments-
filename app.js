81a2de6f97f1fdfeaa952b7e0e470951dd42c112
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:41:52 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,50 +1,28 @@
-//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
-//  If the alien is yellow, print a message that the player earned 10 points.
-var alienColor = "GREEN";
-// If the alien is green, print a message that the player earned 5 points.
-//If the alien is yellow, print a message that the player earned 10 points.
-// If the alien is red, print a message that the player earned 15 points.
-if (alienColor === "GREEN") {
-    console.log("the player just earned 5 points .");
+//Stages of Life: Write an if-else chain that determines a person’s stage of life. 
+// Set a value for the variable age, and then:
+//set a value for the variable age, and then:
+var age = 25;
+// If the person is less than 2 years old, print a message that the person is a baby
+// If the person is at least 2 years old but less than 4, print a message that the person is a toddler
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+// • If the person is age 65 or older, print a message that the person is an elder.
+if (age < 2) {
+    console.log(" person is a baby.");
 }
-else if (alienColor === "yellow") {
-    console.log("the player just earned 10 points  .");
+else if (age >= 2 && age < 4) {
+    console.log("person is a toddler.");
 }
-else if (alienColor === "red") {
-    console.log("the player just earned 15 points .");
+else if (age >= 4 && age < 13) {
+    console.log("person is a kid.");
 }
-else {
-    console.log("please select right color");
+else if (age >= 13 && age < 20) {
+    console.log("person is a teenager.");
 }
-//version 2 the programe
-if (alienColor === "yellow") {
-    console.log("the player just earned 5 points .");
-}
-else if (alienColor === "GREEN") {
-    console.log("the player just earned 10 points .");
-}
-else if (alienColor === "yellow") {
-    console.log("the player just earned 15 points .");
-}
-else if (alienColor === "red") {
-    console.log("the player just earned 15 points .");
+else if (age >= 20 && age < 65) {
+    console.log(" person is an adult.");
 }
 else {
-    console.log("please select right color");
-}
-//version 3 to program
-if (alienColor === "red") {
-    console.log("the player just earned 5 points .");
-}
-else if (alienColor === "GREEN") {
-    console.log("the player just earned 10 points .");
-}
-else if (alienColor === "yellow") {
-    console.log("the player just earned 15 points .");
-}
-else if (alienColor === "red") {
-    console.log("the player just earned 10 points.");
-}
-else {
-    console.log("please select right color");
+    console.log("person is an elder.");
 }
