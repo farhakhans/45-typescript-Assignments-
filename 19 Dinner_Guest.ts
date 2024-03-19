999574b6a2a22cc9818155476ccedbaeb0db1ddf
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:31:52 +0500

    Add files via upload

---
+++ 19 Dinner_Guest.ts
@@ -0,0 +1,6 @@
+// 19 EXCERCISE Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+
+import{ guest_list } from '../14 Guest List/app'
+console.log(` n\ PRINTING NUMBER OF GUEST INVITED`)
+console.log(`we had finally invited ${guest_list.length} from excercise 14`)
--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-// Seeing the World: Think of at least five places in the world you’d like to visit.
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-// alphabectical order 1st project
-var places = ['karachi', 'lahore', 'mirpurkhas', 'peshawar', 'quetta'];
-// console.log('original ' + places);
-// • Print your array in alphabetical order without modifying the actual list.
-// console.log('copy ' +[...places].sort());
-//  Show that your array is still in its original order by printing it.
-//  console.log('original ' + places);
-// • Print your array in reverse alphabetical order without changing the order of the original list
-console.log('unorder ' + __spreadArray([], places, true).sort().reverse());
-// • Show that your array is still in its original order by printing it again.
-console.log('unorder ' + __spreadArray([], places, true).sort().reverse());
-// • Reverse the order of your list. Print the array to show that its order has changed.
-console.log('original ' + places.sort());
-// • Reverse the order of your list again. Print the list to show it’s back to its original order
-console.log('original ' + places.sort().reverse());
+"use strict";
+// 19 EXCERCISE Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_1 = require("../14 Guest List/app");
+console.log(" n PRINTING NUMBER OF GUEST INVITED");
+console.log("we had finally invited ".concat(app_1.guest_list.length, " from excercise 14"));
