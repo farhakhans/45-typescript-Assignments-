e10acdf076e0f3b444ad93a9be797b164440de24
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:26:51 +0500

    Add files via upload

---
+++ 14 Guest_list.ts
@@ -0,0 +1,13 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, 
+// who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+// hen use your list to print a message to each person, inviting them to dinner.
+
+
+// guest list
+
+let guest_list: string [] = ['farha','fouzia','laiba','areeba'];
+for(let i=0; i<guest_list.length; i++){
+    console.log(' \n Respected sir/Madam '  + guest_list[i] + ',\nwe invited you on dinner tomorow.\n\nThank you');
+}
+
+//export{guest_list} 
--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
-//  and make a list that stores several examples.
-//  Use your list to print a series of statements about these items, 
-// such as “I would like to own a Honda motorcycle.”
-// excersie 13 transportaion list 
-var transportaion = ['civic', 'bike', 'bus', 'suzuki', 'coach',];
-for (var i = 0; i < transportaion.length; i++) {
-    console.log("i would like to own a " + transportaion[i]);
+"use strict";
+// Guest List: If you could invite anyone, living or deceased, to dinner, 
+// who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+// hen use your list to print a message to each person, inviting them to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+// guest list
+var guest_list = ['farha', 'fouzia', 'laiba', 'areeba'];
+exports.guest_list = guest_list;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log(' \n Respected sir/Madam ' + guest_list[i] + ',\nwe invited you on dinner tomorow.\n\nThank you');
 }
