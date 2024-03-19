6359e304754734cc2212ee75fd203d4d85eac7b3
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:51:35 +0500

    Add files via upload

---
+++ 39 citiesNmaes.ts
@@ -0,0 +1,11 @@
+// City Names: Write a function called city_country() that takes in the name of a city and its country. 
+//The function should return a string formatted like this:
+
+function cityCountry(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+
+// Example usage:
+console.log(cityCountry("Lahore", "Pakistan"));  // Output: Lahore, Pakistan
+console.log(cityCountry("Bomby", "India"));     // Output: Bomby, India
+console.log(cityCountry("Colombo", "Turki"));      // Output: Colombo, Turki
--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// Cities: Write a function called describe_city() that accepts the name of a city and its country. 
-// The function should print a simple sentence, such as Karachi is in Pakistan. 
-// Give the parameter for the country a default value. Call your function for three different cities, 
-// at least one of which is not in the default country.
-function describe_city(city, country) {
-    if (country === void 0) { country = "Pkaistan"; }
-    console.log("".concat(city, " is in ").concat(country, "."));
+// City Names: Write a function called city_country() that takes in the name of a city and its country. 
+//The function should return a string formatted like this:
+function cityCountry(city, country) {
+    return "".concat(city, ", ").concat(country);
 }
-// Call the function for three different cities
-describe_city("Karachi", "Pakistan"); // Karachi is in Pakistan
-describe_city("Riaz ", "Saudi Arabia"); // Riaz is in Saudia Arabia
-describe_city("Bomby", "India"); // Bomby is in India
+// Example usage:
+console.log(cityCountry("Lahore", "Pakistan")); // Output: Lahore, Pakistan
+console.log(cityCountry("Bomby", "India")); // Output: Bomby, India
+console.log(cityCountry("Colombo", "Turki")); // Output: Colombo, Turki
