28668aa703c62cfaa1c55d8331b7e48affb540f8
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:11:55 +0500

    Add files via upload

---
+++ 3 Famousqoute.ts
@@ -0,0 +1,8 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+//  Your output should look something like the following, including the quotation marks:
+
+
+// storing famous qoute:
+let quote: string= "A person who never made a mistake never tried anything new.";
+let author: string= "Albert Einstein";
+console.log(`${author} once said, "${quote}`);
---
+++ famousqoute.js
@@ -0,0 +1,7 @@
+"use strict";
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+//  Your output should look something like the following, including the quotation marks:
+// storing famous qoute:
+let quote = "A person who never made a mistake never tried anything new.";
+let author = "Albert Einstein";
+console.log(`${author} once said, "${quote}`);
