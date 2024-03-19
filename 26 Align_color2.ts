63dcb2c4e1814f3bc380662b1ba7b9c3a8edf64c
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-03-19 21:40:14 +0500

    Add files via upload

---
+++ 26 Align_color2.ts
@@ -0,0 +1,26 @@
+// Alien Colors #2: Choose a color for an alien as 
+// you did in Exercise 25, and write an if-else chain.
+
+
+//  Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+let alienColor: string = "GREEN";
+
+// write an if-else chain.
+// If the alien’s color is green,
+//print  the player just earned 5 points for shooting the alien.
+if(alienColor ==="GREEN"){
+  console.log("the player just earned 5 points for shooting the alien.");
+
+}else{
+    console.log("the player just earned 10 points.");
+}
+
+//• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+alienColor = "yellow"
+if(alienColor ==="GREEN"){
+    console.log("the player just earned 5 points for shooting the alien.");
+  
+  }else{
+      console.log("the player just earned 10 points.");
+  }
+
--- app.js
+++ app.js
@@ -1,51 +1,22 @@
-// 1st test Tests for equality and inequality with strings
-// string test 
-var car = "civic";
-var age = 25;
-var number = [1, 2, 3, 4];
-// string test 
-//test no 1 equility  (true)
-console.log("Is car == 'civic'? I predict True.");
-console.log(car == 'civic'); //true  (case insenseive)
-// tset no 2  equility  (false)
-console.log("Is car === 'civic'? I predict false.");
-console.log(car === 'civic'); // false (case sensetive)
-//test no 3 equility (true)
-console.log("Is car != 'civic'? I predict true.");
-console.log(car != 'civic');
-//test no 4  equility (false)
-console.log("Is car !== 'civic'? I predict false.");
-console.log(car !== 'civic'); //false (case-sensetive)
-// LOWER FUNCTION TEST
-// TEST 5 LOWER CONVERSION (TRUE)
-console.log("Is car.tolowerCase() == 'civic'? I predict true.");
-console.log(car.toLocaleLowerCase() == 'civic'); // true (converted to lowercase)
-// test 6 lower case conversion (false)
-console.log("Is car.tolowerCase() === 'civic'? I predict false.");
-console.log(car.toLocaleLowerCase() === 'civic');
-//  test 7 numrical test 
-// test equility (true)
-console.log("Is age ==  25? I predict true.");
-console.log(age == 25); // true
-// test 8 inequility (false)
-console.log("Is age !=25? I predict true.");
-console.log(age != 25); // true
-// test 9 greator then (false)
-console.log("Is age >30? I predict false.");
-console.log(age > 30);
-// test 10 less then or equal (true)
-console.log("Is age <=25? I predict true.");
-console.log(age <= 25); //true
-// LoGICAL OPERATORS
-// TEST 11  AND (TRUE)
-console.log("Is age >20 && age <30? I predict true.");
-console.log(age > 20 && age < 30);
-// test 12 OR (FALSE)
-console.log("Is age >30 || age <18? I predict false.");
-console.log(age > 30 || age < 18);
-// test 13 IN ARRY (TRUE)
-console.log("Is in 3 number? i predict true.");
-console.log(3 in number); // true (check for index existence)
-// test 14 not in ARRY (FALSE)
-console.log("Is in 5 number? i predict true.");
-console.log(5 in number);
+"use strict";
+// Alien Colors #2: Choose a color for an alien as 
+// you did in Exercise 25, and write an if-else chain.
+//  Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+let alienColor = "GREEN";
+// write an if-else chain.
+// If the alien’s color is green,
+//print  the player just earned 5 points for shooting the alien.
+if (alienColor === "GREEN") {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
+//• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+alienColor = "yellow";
+if (alienColor === "GREEN") {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
